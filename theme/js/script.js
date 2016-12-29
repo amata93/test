e91bd954e8220e030894b61fd657a1d59d07c669
Andres Mata <andres.mata@softonitg.com>
2016-12-29 09:23:36 -0600

    Test commit

--- theme/js/script.js
+++ theme/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-    //formListener();
+    formListener();
 });
+function formListener() {
+    $('#testForm').on('submit', function (ev) {
+        ev.preventDefault();
+        var object = {
+            email: $('#email').val(),
+            name: $('#name').val(),
+            password: $('#pwd').val()
+        };
+
+        console.log(object);
+
+    });
+}
